4a1094b60f2215fe93b2335cf6ef712e4a52ed01
Rajithareddy4 <velmalarajithareddy1@gmail.com>
2024-12-13 17:16:07 +0530

    completed reactjs

---
+++ myapp/src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+      <marquee>welcome reactjs program</marquee>
+
+    </div>
+    )
+}
+
+export default App
---
+++ myapp1/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h2> Welcome</h2>
+      <h3>Akhi</h3>
+    </div>
+  )
+}
+
+export default App
